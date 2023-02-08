720e96211dd1a70c94f1096f8ddebf81679489b1
Rustem <ryuzikc@gmail.com>
2023-02-08 14:40:11 +0300

    Init

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from '../filter/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
+import { configService } from './config/config.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalFilters(new GlobalExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe());
+  if (!configService.isProduction()) {
+    const document = SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle('Item API')
+        .setDescription('My Item API')
+        .build(),
+    );
+
+    SwaggerModule.setup('docs', app, document);
+  }
+  await app.listen(configService.getPort());
+  console.log(`Server start on port: ${configService.getPort()}`);
+}
+bootstrap();
